45d77688c1905aab6e4d94385346887b263530b7
Dorothy Rhodes <70044671+dorothy-r@users.noreply.github.com>
2020-12-10 10:44:07 -0500

    add accordion and collapsible nav

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function myFunction() {
+    let x = document.getElementById("topnavMenu");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");   
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
 let fields = {};
 
 document.addEventListener("DOMContentLoaded", function() {
